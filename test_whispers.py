70fcecb63f77a0ed43e8666735322d6a8ce9a2b6
Alwin <alwinng420@gmail.com>
2026-07-17 01:31:28 -0400

    initial whispers infrastructure, create inital nodes and graph from vectors + names

---
+++ test_whispers.py
@@ -0,0 +1,24 @@
+from whispers import Whispers, cos_dist
+import numpy as np
+
+testing_segment = 1
+
+vecs = [np.asarray([1,2,3]),
+        np.asarray([1,2,3]),
+        np.asarray([5,6,7]),
+        ]
+
+names = ["a","b","c"]
+
+threshold = 0.01
+
+w = Whispers(vecs,names,threshold)
+
+if testing_segment == 1:
+    w.create_matrix()
+    w.create_nodes()
+    w.get_plot()
+    
+elif testing_segment == 2:
+    print(cos_dist(vecs[0],vecs[2]))
+    print(vecs[0].shape)

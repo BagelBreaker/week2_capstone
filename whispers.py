356e4753794b5f864e4a16a769490fdf0548252c
Alwin <alwinng420@gmail.com>
2026-07-17 10:45:08 -0400

    cluthering function completed. todo: improve time complexity of helper funcs, test on real data

--- whispers.py
+++ whispers.py
@@ -150,10 +150,36 @@ class Whispers:
 
     def whispers_step(self):
         selected_node = random.choice(self.nodes)
+        old_label = selected_node.get_label()
         counts = Counter()
         neighbors = selected_node.get_neighbors()
-        for neighbor in neighbors:
-            counts[self.nodes[neighbor].get_label()] += 1
-        max_count = max(counts.values())
-        new_label = random.choice([id for id, count in counts.items() if count == max_count])
-        selected_node.set_label(new_label)
+        if len(neighbors) > 0:
+            for neighbor in neighbors:
+                counts[self.nodes[neighbor].get_label()] += 1
+            max_count = max(counts.values())
+            new_label = random.choice([id for id, count in counts.items() if count == max_count])
+            selected_node.set_label(new_label)
+        else:
+            new_label = old_label
+        return old_label, new_label
+
+    def train(self, convergence_threshold=10, stopping_threshold=1000): # only run this once
+        label_counts = Counter()
+        total_labels = len(self.nodes)
+        for node in self.nodes:
+            label_counts[node.get_label()] += 1
+        idle_iterations = 0
+        total_iterations = 0
+        while idle_iterations < convergence_threshold and total_iterations < stopping_threshold:
+            old_label, new_label = self.whispers_step()
+            label_counts[old_label] -= 1
+            label_counts[new_label] += 1
+            if label_counts[old_label] == 0:
+                total_labels -= 1
+                idle_iterations = 0
+            else:
+                idle_iterations += 1
+            total_iterations += 1
+            self.get_plot()
+        
+            
